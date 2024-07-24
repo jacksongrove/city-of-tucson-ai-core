41c2b1576b64cf220fba08128deaf59a0bbec9ba
hector786g8 <arceh@arizona.edu>
2024-07-24 14:13:22 -0700

    new line

--- product/responses.py
+++ product/responses.py
@@ -12,6 +12,8 @@ def get_response(user_input: str) -> str:
         return 'Good, thanks!'
     elif 'bye' in lowered:
         return 'See you!'
+    elif 'goodbye' in lowered:
+        return 'See you next time!!!!!!'
     elif 'roll dice' in lowered:
         return f'You rolled: {randint(1, 6)}'
     else:
