90d25ba3d8efc277b850748ffaa63ad186a62e60
hector786g8 <arceh@arizona.edu>
2024-07-25 14:11:26 -0700

    syllabus in response

--- product/responses.py
+++ product/responses.py
@@ -1,5 +1,6 @@
 from random import choice, randint
-
+from shared import syllabus_content  # Import shared module
+import re
 
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
@@ -16,7 +17,16 @@ def get_response(user_input: str) -> str:
         return 'See you next time!!!!!!'
     elif 'roll dice' in lowered:
         return f'You rolled: {randint(1, 6)}'
-    else:
-        return choice(['I do not understand...',
-                       'What are you talking about?',
-                       'Do you mind rephrasing that?'])
+
+    # Check for syllabus-related questions
+    for key, value in syllabus_content.items():
+        if 'syllabus' in lowered or 'schedule' in lowered or 'deadline' in lowered:
+            if re.search(lowered, value, re.IGNORECASE):
+                return value
+            else:
+                return "Sorry, I couldn't find any relevant information in the syllabus."
+
+    # Default responses
+    return choice(['I do not understand...',
+                   'What are you talking about?',
+                   'Do you mind rephrasing that?'])
